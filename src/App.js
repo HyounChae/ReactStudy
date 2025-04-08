64c7b4f14201031deaf4a67580c2aa3a5bf54a5a
이현채 <a01099593510@gmail.com>
2025-04-09 02:02:26 +0900

    ?? 겉 모습은 똑같은 것 같은데,,,

--- src/App.js
+++ src/App.js
@@ -5,79 +5,79 @@ function App() {
   const click = () => {
     console.log("click");
   };
+
   return (
-    <div>
-      <div className="header">
-        <a className="logo" href="/">
-          everyuneez 학생복지스토어
-        </a>
-        <div className="nav">
-          <a className="nav__search" href="/검색">
-            검색
-          </a>
-          <a className="nav__login" href="/로그인">
-            로그인
-          </a>
-          <a className="nav__my" href="/마이페이지">
-            마이페이지
-          </a>
-          <a className="nav__cart" href="/장바구니">
-            장바구니
-          </a>
+    <>
+    {/*<!-- Header -->*/}
+    <header>
+        <div className="header_container">
+            <a className="logo" href="/">
+              {/* <!-- everyuneez 학생복지스토어 -->*/}
+            </a>
+            <nav className="account account_notlogin">
+                <a className="account_search" href="/search">
+                검색
+                </a>
+                <a className="account_login" href="/login">
+                로그인
+                </a>
+                <a className="account_mypage" href="/마이페이지">
+                마이페이지
+                </a>
+                <a className="account_cart" href="/장바구니">
+                장바구니
+                </a>
+            </nav>
         </div>
-      </div>
-
-      <hr></hr>
-
-      <div>
-        <div className="nav__container">
-          <div className="nav">
-            <div className="nav__left">
-              <div className="nav_leftbutton">
-              <a class="nav__digital" data-url_path="digital">
-                디지털
-              </a>
-              <a class="nav__computer" data-url_path="computer">
-                컴퓨터
-              </a>
-              <a class="nav__electronic" data-url_path="electronic">
-                가전
-              </a>
-              <a class="nav__beauty" data-url_path="beauty">
-                뷰티
-              </a>
-              <a class="nav__fashion" data-url_path="fashion">
-                패션
-              </a>
-              <a class="nav__fashionacc" data-url_path="fashionacc">
-                패션잡화
-              </a>
-              <a class="nav__smartphone" data-url_path="smartphone">
-                스마트폰
-              </a>
-              <a class="nav__lifestyle" data-url_path="lifestyle">
-                라이프스타일
-              </a>
-              </div>
+    </header>
+    
+       {/* Container */}
+    <div className="nav_container">
+        <div class="nav">
+            {/* <!-- 왼쪽 --> */}
+            <div className="nav_left">
+                <div className="nav_leftbutton" data-url-path="digital">
+                    디지털
+                </div>
+                <div className="nav_leftbutton" data-url-path="computer">
+                    컴퓨터
+                </div>
+                <div className="nav_leftbutton" data-url-path="electronic">
+                    가전
+                </div>
+                <div className="nav_leftbutton" data-url-path="beauty">
+                    뷰티
+                </div>
+                <div className="nav_leftbutton" data-url-path="fashion">
+                    패션
+                </div>
+                <div className="nav_leftbutton" data-url-path="fashionacc">
+                    패션잡화
+                </div>
+                <div className="nav_leftbutton" data-url-path="smartphone">
+                    스마트폰
+                </div>
+                <div className="nav_leftbutton" data-url-path="lifestlye">
+                    라이프스타일
+                </div>
             </div>
 
-            <div class="nav__right">
-              <div class="nav_rightbutton">
-              <a class="nav__content" href="/content">
-                콘텐츠
-              </a>
-              <a class="nav__event" href="/event">
-                혜택/이벤트
-              </a>
-              <a class="nav__brand" href="/brand">
-                브랜드
-              </a>
-              </div>
+            {/*<!-- 오른쪽 -->*/}
+            <div className="nav_right">
+                <div className="nav_rightbutton" href="content">
+                    콘텐츠
+                </div> 
+                <div className="nav_rightbutton" href="event">
+                    혜택/이벤트
+                </div>
+                <div className="nav_rightbutton" href="brand">
+                    브랜드
+                </div>
             </div>
-          </div>
-        </div>
+
         </div>
     </div>
+    </>
 
   );
 }
